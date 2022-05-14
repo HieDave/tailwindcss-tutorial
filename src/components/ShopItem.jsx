6c97da24b2bdb351cfe3019a6a3227f165f305fd
Dave <davidsibanda776@gmail.com>
2022-05-14 17:57:59 +0200

    edited shopList

--- src/components/ShopItem.jsx
+++ src/components/ShopItem.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import shoeImage from '../img/shoe.png'
 
-const ShopItem = () => {
+const ShopItem = ({props}) => {
+    const { name, type, price } = props
   return (
-    <div>ShopItem</div>
+    <div className='relative p-6 bg-white rounded-lg'>
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 absolute top-4 right-4 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+            <path strokeLinecap="round" strokeLinejoin="round" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+        </svg>
+        <div className='flex justify-start items-center'>
+            <img src={shoeImage} alt="shoe" />
+        </div>
+        <div className='text-base mt-8'>{name}</div>
+        <div className='text-gray-400'>{type}</div>
+        <div>ratings</div>
+        <div className='text-lg text-black mt-2'>{`$${price}`}</div>
+    </div>
   )
 }
 
--- src/components/ShopList.jsx
+++ src/components/ShopList.jsx
@@ -1,7 +1,59 @@
 import React from 'react'
-import shoe from '../img/shoe.png'
+import ShopItem from './ShopItem'
 
 const ShopList = () => {
+    const items = [
+        {
+            name: 'Nike',
+            type: 'Air Force',
+            price: '115',
+        },
+        {
+            name: 'Nike',
+            type: 'Air Force',
+            price: '115',
+        },  
+        {
+            name: 'Nike',
+            type: 'Air Force',
+            price: '115',
+        },
+        {
+            name: 'Nike',
+            type: 'Air Force',
+            price: '115',
+        },
+        {
+            name: 'Nike',
+            type: 'Air Force',
+            price: '115',
+        },
+        {
+            name: 'Nike',
+            type: 'Air Force',
+            price: '115',
+        },
+        {
+            name: 'Nike',
+            type: 'Air Force',
+            price: '115',
+        },
+        {
+            name: 'Nike',
+            type: 'Air Force',
+            price: '115',
+        },
+        {
+            name: 'Nike',
+            type: 'Air Force',
+            price: '115',
+        },
+        {
+            name: 'Nike',
+            type: 'Air Force',
+            price: '115',
+        }
+    ]
   return (
     <div className='col-span-3 bg-customGray p-4'>
         <div className='flex p-1 items-center'>
@@ -9,19 +61,12 @@ const ShopList = () => {
             <div className='flex items-center justify-center rounded-2xl border-2 border-primary px-4 py-1 mx-2 text-pink-600 bg-pink-100'>Price</div>
             <div className='flex items-center justify-center rounded-2xl border-2 border-gray-400 px-4 py-1 mx-2 text-gray-400'>Date</div>
         </div>
-        <div className='grid grid-cols-4 mt-2'>
-            <div className='relative p-6 bg-white rounded-lg'>
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 absolute top-4 right-4 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
-                </svg>
-                <div className='flex justify-start items-center'>
-                    <img src={shoe} alt="shoe"/>
-                </div>
-                <div className='text-base mt-8'>Nike</div>
-                <div className='text-gray-400'>Air Force 1</div>
-                <div>ratings</div>
-                <div className='text-lg text-black mt-2'>$115</div>
-            </div>
+        <div className='grid grid-cols-4 mt-2 gap-2'>
+            {items.map((object, index) => {
+                return (
+                    <ShopItem props={object} />
+                )
+            })}
         </div>
     </div>
   )

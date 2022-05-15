73e3529942a2fff3a65a0ee4b6c966ce44326ed4
Dave <davidsibanda776@gmail.com>
2022-05-15 19:36:38 +0200

    changed-search-filter

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,9 +8,16 @@ export const Sidebar = () => {
   return (
     <div className='w-1/4 sticky inset-y-0 left-0 bg-customWhite py-6 px-12 text-2xl'>
       Filters
-      <div className='bg-gray-200 text-sm p-2 my-2 text-gray-500 rounded'>
-        Search filters...
-      </div>
+      <form class="flex items-center my-2">
+        <label for="simple-search" class="sr-only">Search</label>
+        <div class="relative w-full">
+            <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+                <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
+            </div>
+            <input type="text" id="simple-search" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-50 dark:border-gray-300 dark:placeholder-gray-400 dark:text-gray-900 dark:focus:ring-primary dark:focus:border-primary" placeholder="Search filters..." required />
+        </div>
+        <button type="submit" class="p-2.5 ml-2 text-sm font-medium text-white bg-primary rounded-lg border border-primary hover:bg-primary focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-pink-400 dark:hover:bg-pink-500 dark:focus:ring-pink-600"><svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg></button>
+      </form>
       <Price />
       <Producer />
       <Rating />
